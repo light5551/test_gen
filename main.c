1c4c0dce4053f2cea7608ecee3a5818195085b81
light5551 <sergey.glazunov.99@mail.ru>
2019-03-18 20:37:18 +0300

    gdg

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int num[10] = {
+1,3,6,5,8,7,9,6,2,0
+};
+
+int comp (const int *, const int *);
+
+int main(void)
+{
+int i;
+printf("Original array: ");
+for (i=0; i<10; i + +) printf("%d ",num[i]);
+printf ("\n");
+qsort(num, 10, sizeof (int), (int(*) (const void *, const void *)) comp);
+printf("Sorted array: ");
+for(i = 0; i <10; i + + ) printf("%d ", num[i]);
+return 0;
+}
+
+/* сравнение двух целых */
+int comp (const int *i, const int *j)
+{
+return *i - *j;
+}
