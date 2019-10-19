2854053070752c3076f21b9648402f3eec48d760
light5551 <sergey.glazunov.99@mail.ru>
2019-10-19 20:45:18 +0000

    for anna 6

---
+++ index.js
@@ -0,0 +1,22 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+var test;
+module.exports.request = function() {
+    let req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+
+	if(this.readyState == 4 && this.status == 200)
+		{
+		test = this.responseText;
+		post();
+		};
+	};
+req.open("GET",`http:\/\/localhost:3000`, true);
+req.send();
+
+}
+
+function post(){
+let req = new XMLHttpRequest();
+req.open("POST", `http:\/\/localhost:3000/${test}`, true);
+req.send();
+}
