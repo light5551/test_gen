189e80c0eb71eaea04855f59f62e6639c576aa9f
light5551 <31547459+light5551@users.noreply.github.com>
2020-05-22 14:29:02 +0300

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int num[10] = {
+1,3,6,5,8,7,9,6,2,0
+};
+
+int comp (const int *, const int *);
+//some test for prs
+int main(void)
+{
+test1
+test4
+int i;
+printf("Original array: ");
+for (i=0; i<10; i + +) printf("%d ",num[i]);
+printf ("\n");
+qsort(num, 10, sizeof (int), (int(*) (const void *, const void *)) comp);
+printf("Sorted array: ");
+for(i = 0; i <10; i + + ) printf("%d ", num[i]);
+return 0;
+}
+
+/* сравнение двух целых */
+int comp (const int *i, const int *j)
+{
+return *i - *j;
+}
